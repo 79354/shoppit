d29c2f78fbb0e3a27d74b0d8b03c7d0de790f57c
79354 <gillramansingh900@gmail.com>
2025-12-07 20:27:27 +0530

    websocket routing

--- backend/support/routing.py
+++ backend/support/routing.py
@@ -1,1 +1,6 @@
-#websocket routing
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/support/chat/(?P<room_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/support/notifications/$', consumers.NotificationConsumer.as_asgi()),
+]
